cce312cfa8a7a28596f709ea0b3b0e29898ac3ea
Devinderjeet <er_devinderjeet@hotmail.com>
2018-09-07 12:15:14 +0530

    Revert "Merge branch 'master' into developer"
    This reverts commit a846d1a6b6949f5c0782222f396681a3bd0a4f9a, reversing
    changes made to 15010d2c1cbf8755fe6c1265fa369602df5e0bfa.

---
+++ backoffice/index.php
@@ -0,0 +1,13 @@
+<?php 
+header("Location: login.php"); /* Redirect browser */
+ ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
